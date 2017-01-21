0e6e59c7cc913edf04ff3d7f76ca25269d0355a0
b-all-721 <brenallen1.1x10e11@gmail.com>
2017-01-21 16:22:14 -0500

    Additional Classes
    Added .cs files:
    Driver
    SubscriptionList...

---
+++ SolutionFolder/PublisherApp/SubscriptionList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherApp
+{
+    class SubscriptionList
+    {
+        public static List<SendViaEmail> emailList = new List<SendViaEmail>();
+
+
+        public static List<string> phoneList = new List<string>();
+    }
+}
