2b6a09e6d92568b27f1b2b6cfe489c73cbf8c83f
b-all-721 <brenallen1.1x10e11@gmail.com>
2017-01-21 22:24:27 -0500

    Subscription by email and by phone added

--- SolutionFolder/PublisherApp/FormSubscription.cs
+++ SolutionFolder/PublisherApp/FormSubscription.cs
@@ -17,20 +17,37 @@ namespace PublisherApp
             InitializeComponent();
         }
 
-        private void button_subscribe_Click(object sender, EventArgs e)
+        private void processEmail()
         {
             // For email subscription.
             if (checkBox_sentByMail.Checked)
             {
+                string email = textBox_email.Text;
+
                 // Check if email is valid.
-                if (textBox_email.Text == "")
+                if (email == "")
                 {
                     MessageBox.Show("Email can't be empty.");
                 }
                 else
                 {
+                    //// Creat a new SendViaEmail object for checking of its existence.
+                    //SendViaEmail sendViaEmailObjTest = new SendViaEmail(email);
+
+                    // Check existence.
+                    foreach (SendViaEmail item in Program.sendViaEmailList)
+                    {
+                        if (item.getEmailAddr() == email)
+                        {
+                            //
+                            MessageBox.Show(email + " is already subscribed.");
+                            return;
+                        }
+                    }
+
+                    // If the code gets here, create a new email subscription.
                     // Creat a new SendViaEmail object.
-                    SendViaEmail sendViaEmailObj = new SendViaEmail(textBox_email.Text);
+                    SendViaEmail sendViaEmailObj = new SendViaEmail(email);
 
                     // Subscribe.
                     sendViaEmailObj.Subscribe(Program.publisher);
@@ -39,13 +56,61 @@ namespace PublisherApp
                     Program.sendViaEmailList.Add(sendViaEmailObj);
 
                     //
-                    MessageBox.Show(textBox_email.Text + " subscribed.");
+                    MessageBox.Show(email + " subscribed.");
                 }
-
             }
+        }
 
-
+        private void processMobile()
+        {
             // For mobile subscription.
+            if (checkBox_sentByMobile.Checked)
+            {
+                string mobile = textBox_mobile.Text;
+
+                // Check if mobile is valid.
+                if (mobile == "")
+                {
+                    MessageBox.Show("Mobile can't be empty.");
+                }
+                else
+                {
+                    //// Creat a new SendViaMobile object for checking of its existence.
+                    //SendViaMobile sendViaMobileObjTest = new SendViaMobile(mobile);
+
+                    // Check existence.
+                    foreach (SendViaMobile item in Program.sendViaMobileList)
+                    {
+                        if (item.getMobile() == mobile)
+                        {
+                            //
+                            MessageBox.Show(mobile + " is already subscribed.");
+                            return;
+                        }
+                    }
+
+                    // If the code gets here, create a new mobile subscription.
+                    // Creat a new SendViaMobile object.
+                    SendViaMobile sendViaMobileObj = new SendViaMobile(mobile);
+
+                    // Subscribe.
+                    sendViaMobileObj.Subscribe(Program.publisher);
+
+                    // Add the mobile from the textbox to the mobile list.
+                    Program.sendViaMobileList.Add(sendViaMobileObj);
+
+                    //
+                    MessageBox.Show(mobile + " subscribed.");
+                }
+            }
+        }
+
+        private void button_subscribe_Click(object sender, EventArgs e)
+        {
+            processEmail();
+            processMobile();
+
+
         }
     }
 }
