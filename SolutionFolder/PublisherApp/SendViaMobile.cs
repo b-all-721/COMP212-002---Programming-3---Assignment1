5e8613c18b663694507d36828b9075004e6c93a5
b-all-721 <brenallen1.1x10e11@gmail.com>
2017-01-21 23:12:33 -0500

    SendViaMobile.class fix

--- SolutionFolder/PublisherApp/SendViaMobile.cs
+++ SolutionFolder/PublisherApp/SendViaMobile.cs
@@ -3,38 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PublisherApp
 {
-          public class SendViaMobile
-          {
-              private String cellPhone;
+    public class SendViaMobile
+    {
+        private String cellPhone;
 
-              public SendViaMobile() { }
+        public SendViaMobile() { }
 
-              public SendViaMobile(String phone)
-              {
-                  cellPhone = phone;
-              }
+        public SendViaMobile(String phone)
+        {
+            cellPhone = phone;
+        }
 
-              public void setMobile(String phone)
-              {
-                  cellPhone = phone;
-              }
+        public void setMobile(String phone)
+        {
+            cellPhone = phone;
+        }
 
-              public String getMobile()
-              {
-                  return cellPhone;
-              }
+        public String getMobile()
+        {
+            return cellPhone;
+        }
 
-              private void sendMessage(string msg)
-              {
-                  Console.WriteLine("The message "+ "\"" + msg+ "\" has already texted to " + cellPhone );
-              }
+        private void sendMessage(string msg)
+        {
+            //Console.WriteLine("The message "+ "\"" + msg+ "\" has already texted to " + cellPhone );
+            MessageBox.Show("The message" + "\"" + msg + "\" has already been sent to " + cellPhone);
+        }
 
-              public void Subscribe(Publisher pub)
-              {
-                  pub.publishmsg += sendMessage;
-              }
-          }
+        public void Subscribe(Publisher pub)
+        {
+            pub.publishmsg += sendMessage;
+        }
+    }
 }
